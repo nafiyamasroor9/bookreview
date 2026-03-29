5304326cc8d8f2fe9de628fd39c9afa52eac38b4
nafiyamasroor9 <nafiiiyahas1@gmail.com>
2026-03-29 19:14:59 +0530

    try

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
     import axios from 'axios';
 
     const API = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE || 'https://inkwell-leob.onrender.com',
+    baseURL: import.meta.env.VITE_API_BASE || 'https://bookreview-vwnp.onrender.com',
     });
 
     API.interceptors.request.use(config => {
@@ -12,3 +12,5 @@
     });
 
     export default API;
+
+    // baseURL: import.meta.env.VITE_API_BASE || 'https://inkwell-leob.onrender.com',
